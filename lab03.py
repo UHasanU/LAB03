fa4c9291e9f503290cf13b90c09297b11d03c315
UHasanU <mualimha@gmail.com>
2024-09-15 14:55:50 +0300

    Add a method that reads from stdin a list of students and record them

--- lab03.py
+++ lab03.py
@@ -1,12 +1,18 @@
 class Lab03:
     def __init__(self, initial_count):
         self.count = initial_count
+        self.students = []
 
     def add(self, x):
         self.count += x
 
     def get_current_count(self):
         return self.count
+
+    def read_student_list(self):
+        students = input("Enter the list of students seperated by comma: ").split(",")
+        self.students.extend(students)
+        self.add(len(students))
 
 
 def main():
